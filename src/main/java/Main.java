982f1fae4d573a6f0726376fe133b9430dc40799
kr3kenward3n <egorzelikov2015@mail.ru>
2024-10-18 00:18:55 +0300

    Добавил ломбок и изменил структуру сущностей в классе CsvData. update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,19 +5,35 @@ import parsing.CsvParser;
 import parsing.JsonParser;
 import parsing.WebPageParser;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException, CsvValidationException {
+
         WebPageParser webPageParser = new WebPageParser();
         List<MetroLine> metroLines = webPageParser.parseMetroLines("https://skillbox-java.github.io/");
         List<MetroStation> metroStations = webPageParser.parseMetroStations("https://skillbox-java.github.io/");
 
         FileSearcher fileSearcher = new FileSearcher();
-        List<File> files = fileSearcher.searchFiles("C:/Users/Elena/Desktop/stations/stations-data");
+        List<File> files = fileSearcher.searchFiles("C:\\Users\\Elena\\Desktop\\Java");
+
+        FileReader fr = new FileReader("text.txt");
+        BufferedReader reader = new BufferedReader(fr);
+        List<String> lines = new ArrayList<>();
+
+        String line;
+        while ((line = reader.readLine()) != null) {
+            lines.add(line);
+        }
+
+        for (String temp : lines) {
+            System.out.println(temp);
+        }
 
         JsonParser jsonParser = new JsonParser();
         List<JsonData> jsonData = new ArrayList<>();
--- src/main/java/parsing/JsonParser.java
+++ src/main/java/parsing/JsonParser.java
@@ -10,15 +10,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class JsonParser {
+
     public List<JsonData> parseJsonFile(File file) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
         JsonNode node = mapper.readTree(file);
         List<JsonData> data = new ArrayList<>();
-        for (JsonNode jsonNode : node) {
+
+        for (JsonNode jsonNode : node)
+        {
             String name = jsonNode.get("name").asText();
             int lineNumber = jsonNode.get("lineNumber").asInt();
             data.add(new JsonData(name, lineNumber));
         }
+
         return data;
     }
 }
